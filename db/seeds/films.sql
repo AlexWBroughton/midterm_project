67fec2d412d763c09d2d6ba4123e10a7d871b8e0
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-29 04:32:33 +0000

    adds seeds to databases

---
+++ db/seeds/films.sql
@@ -0,0 +1,15 @@
+--poplulating the film table       YYYY-MM-DD
+
+INSERT INTO films (name, date_added, length)
+VALUES ( 'Interstellar', '2023-07-26', 169),
+( 'Fight Club', '2023-07-26', 139),
+('The Matrix', '2022-06-06', 136),
+('Toy Story', '2023-07-27', 81),
+('Finding Nemo', '2023-07-28', 100),
+('Spirited Away', '2023-09-28', 125),
+('The Incredibles', '2023-11-29', 115),
+('WALL-E', '2023-07-29', 98);
+
+
+
+
