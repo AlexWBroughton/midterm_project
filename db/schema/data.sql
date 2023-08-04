8373a2e275cb93f89102b446d2f100478817eb11
jasminehilton <jasminehilton1@live.com>
2023-08-03 19:45:30 -0700

    Added users table in data.sql file

--- db/schema/data.sql
+++ db/schema/data.sql
@@ -1,3 +1,21 @@
+-- Drop and recreate tables
+DROP TABLE IF EXISTS categories CASCADE;
+CREATE TABLE categories(
+  id INTEGER PRIMARY KEY NOT NULL,
+  category VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS todos CASCADE;
+CREATE TABLE todos(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name_of_todo VARCHAR(255),
+  category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  completed BOOLEAN NOT NULL,
+  date_added DATE NOT NULL,
+  date_completed DATE
+);
+
+
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
