46df1f93cb7af691570a677b34157c2eb8d3d950
jasminehilton <jasminehilton1@live.com>
2023-08-03 19:29:56 -0700

    created users table and users seeds

--- db/schema/users.sql
+++ db/schema/users.sql
@@ -1,15 +1,7 @@
--- Drop and recreate tables
-DROP TABLE IF EXISTS categories CASCADE;
-CREATE TABLE categories(
-  id INTEGER PRIMARY KEY NOT NULL,
-  category VARCHAR(255) NOT NULL
-);
-
-DROP TABLE IF EXISTS todos CASCADE;
-CREATE TABLE todos(
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name_of_todo VARCHAR(255),
-  category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE,
-  completed BOOLEAN NOT NULL,
-  date_added DATE NOT NULL
+  name VARCHAR(255) ,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
---
+++ db/seeds/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id,name, email, password)
+VALUES('1','Bob', 'a@a.com', 123),
+('2','Sally', 'b@b.com', 123);
